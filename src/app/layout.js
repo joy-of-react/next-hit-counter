3e5cdcb954fa1ce97a8eeb64a44075d27866fa87
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-05 17:59:20 -0400

    Initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css';
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <footer>
+          Page rendered on{' '}
+          {new Date().toLocaleString()}
+        </footer>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DATABASE_PATH = '/src/database.json';
+
+function Home() {
+  return (
+    <main>
+      <h1>Welcome!</h1>
+      <p>You are visitor number X.</p>
+    </main>
+  );
+}
+
+export default Home;
