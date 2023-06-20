51420a4ffe034239a82db0cedf4367371444bfa4
Joshua Comeau <joshwcomeau@gmail.com>
2023-06-20 17:57:23 -0400

    Add clearer instructions for exercise

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
+import {
+  readFile,
+  writeFile,
+} from '../helpers/file-helpers';
+
 const DATABASE_PATH = '/src/database.json';
+
+/*
+`readFile` takes 1 argument:
+• the path to the file:
+
+readFile('/path/to/file');
+
+`writeFile` takes 2 arguments:
+• The path to the file
+• The new contents for the file
+
+writeFile(
+  '/path/to/file',
+  '{ "hello": "world" }'
+);
+*/
 
 function Home() {
   return (
