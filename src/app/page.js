c9016287481d800d6e3cf3f7b35ffe3f8691def7
László Czöndör <czondi@hotmail.com>
2023-09-19 22:24:24 +0200

    feat(page): show visitor number

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import { readFile, writeFile } from "../helpers/file-helpers";
 
-const DATABASE_PATH = '/src/database.json';
-
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
+const DATABASE_PATH = "/src/database.json";
 
 function Home() {
+  const db = JSON.parse(readFile(DATABASE_PATH));
+  db.hits++;
+  writeFile(DATABASE_PATH, JSON.stringify(db));
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {db.hits}.</p>
     </main>
   );
 }
